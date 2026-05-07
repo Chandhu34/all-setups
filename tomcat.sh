11a263282a1cfc02158c5aca6379d2b6a96fcc81
Dasari Chandana <chandanadasari34@gmail.com>
2026-05-07 10:50:00 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,5 +1,5 @@
 yum install java-21-amazon-corretto -y
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v10.1.35/bin/apache-tomcat-10.1.35.tar.gz
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.35/bin/apache-tomcat-10.1.35.tar.gz
 tar -zxvf apache-tomcat-10.1.35.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-10.1.35/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-10.1.35/conf/tomcat-users.xml
