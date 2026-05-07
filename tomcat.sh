b5b60e66816e8be125fb09b6a0ab9a779b00a056
Dasari Chandana <chandanadasari34@gmail.com>
2026-05-07 10:51:03 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,4 +1,4 @@
-yum install java-21-amazon-corretto -y
+yum install java-17-amazon-corretto -y
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.35/bin/apache-tomcat-10.1.35.tar.gz
 tar -zxvf apache-tomcat-10.1.35.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-10.1.35/conf/tomcat-users.xml
